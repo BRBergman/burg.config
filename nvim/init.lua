df88312e58cec8b5f4f841a8ff833fad838521d7
BRBergman <50304305+BRBergman@users.noreply.github.com>
2026-04-14 20:46:59 -0400

    nvim config

---
+++ nvim/init.lua
@@ -0,0 +1,167 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+vim.lsp.enable('rust-analyser')
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+-- provided by rust-analyzer.
+--vim.g.lazyvim_rust_diagnostics = "rust-analyzer"
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+	{ "rose-pine/neovim", name = "rose-pine" },
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{
+  "Saecki/crates.nvim",
+  event = { "BufRead Cargo.toml" },
+  opts = {
+    completion = {
+      crates = {
+        enabled = true,
+      },
+    },
+    lsp = {
+      enabled = true,
+      actions = true,
+      completion = true,
+      hover = true,
+    },
+  },
+},
+{
+  "nvim-treesitter/nvim-treesitter",
+  opts = { ensure_installed = { "rust", "ron" } },
+},
+{
+  'mrcjkb/rustaceanvim',
+  -- To avoid being surprised by breaking changes,
+  -- I recommend you set a version range
+  version = '^9',
+  -- This plugin implements proper lazy-loading (see :h lua-plugin-lazy).
+  -- No need for lazy.nvim to lazy-load it.
+  lazy = false,
+},
+{
+  "luukvbaal/statuscol.nvim",
+  config = function()
+	  vim.o.number = true
+vim.o.relativenumber = true
+    -- Custom function to show both absolute and relative line numbers
+    local function lnum_both()
+      local lnum = vim.v.lnum
+      local relnum = vim.v.lnum == vim.fn.line(".") and 0 or math.abs(vim.v.lnum - vim.fn.line("."))
+      return string.format("│%3d %2d│", lnum, relnum)
+    end
+    require("statuscol").setup({
+      setopt = true,
+      segments = {
+        {
+          sign = {
+            namespace = { "gitsigns.*" },
+            name = { "gitsigns.*" },
+          },
+        },
+        {
+          sign = {
+            namespace = { ".*" },
+            name = { ".*" },
+            auto = true,
+          },
+        },
+        {
+          text = { lnum_both, " " },
+          condition = { true },
+          click = "v:lua.ScLa",
+        },
+      },
+    })
+  end,
+},
+{
+    "MysticalDevil/inlay-hints.nvim",
+    event = "LspAttach",
+    dependencies = { "neovim/nvim-lspconfig" }, -- optional
+    config = function()
+      require("inlay-hints").setup()
+    end,
+  },
+  {
+  'saghen/blink.cmp',
+  -- optional: provides snippets for the snippet source
+  dependencies = { 'rafamadriz/friendly-snippets' },
+
+  -- use a release tag to download pre-built binaries
+  version = '1.*',
+  -- AND/OR build from source
+  -- build = 'cargo build --release',
+  -- If you use nix, you can build from source with:
+  -- build = 'nix run .#build-plugin',
+
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
+  opts = {
+    -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+  --   'super-tab',-- for mappings similar to vscode (tab to accept)
+    -- 'enter',-- for enter to accept
+    -- 'none' for no mappings
+    --
+    -- All presets have the following mappings:
+    -- C-space: Open menu or open docs if already open
+    -- C-n/C-p or Up/Down: Select next/previous item
+    -- C-e: Hide menu
+    -- C-k: Toggle signature help (if signature.enabled = true)
+    --
+    -- See :h blink-cmp-config-keymap for defining your own keymap
+    keymap = { preset = 'super-tab' },
+
+    appearance = {
+      -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+      -- Adjusts spacing to ensure icons are aligned
+      nerd_font_variant = 'mono'
+    },
+
+    -- (Default) Only show the documentation popup when manually triggered
+    completion = { documentation = { auto_show = true } },
+
+    -- Default list of enabled providers defined so that you can extend it
+    -- elsewhere in your config, without redefining it, due to `opts_extend`
+    sources = {
+      default = { 'lsp', 'path', 'snippets', 'buffer' },
+    },
+
+    -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+    -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+    -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+    --
+    -- See the fuzzy documentation for more information
+    fuzzy = { implementation = "prefer_rust_with_warning" }
+  },
+  opts_extend = { "sources.default" }
+}
+
+
+-- add your plugins here
+  },
+  -- Configure any other settings here. See the documentation for more details.
+  -- colorscheme that will be used when installing plugins.
+  install = { colorscheme = { "habamax" } },
+  -- automatically check for plugin updates
+  checker = { enabled = true },
+})
+vim.cmd("colorscheme catppuccin-latte")
