7554327931b76e06a1c05b2e106f3ca427cc938d
BRBergman <burg@burgburg.net>
2026-08-07 14:08:54 -0400

    hyprland is now lua

--- hypr/start.sh
+++ hypr/start.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
-swww-daemon&
+awww-daemon&
 
 #swww img ~/.config/hypr/wallpaper.jpg&
-swww img ~/.config/hypr/nix-Wallpaper.png&
+awww img ~/.config/hypr/nix-Wallpaper.png&
 
 nm-applet --indicator &
 
-waybar &
-protonvpn-app&
 
 dunst
--- waybar/proton.sh
+++ waybar/proton.sh
@@ -5,11 +5,13 @@
 #SPECIALVPNWS= hyprctl dispatch movetoworkspacesilent "special:vpn,class:.protonvpn-app-wrapped"
 
 #echo $CURRENTWS
-protoncurrentpage=$(hyprctl -j clients | jq 'map(select(.class == ".protonvpn-app-wrapped"))[0].workspace.id')
+protoncurrentpage=$(hyprctl -j clients | jq 'map(select(.class == "proton.vpn.app.gtk"))[0].workspace.id')
 usercurrentpage=$(hyprctl -j activeworkspace | jq '.id')
 if [ "$protoncurrentpage" -eq "$usercurrentpage" ]; then
-hyprctl dispatch movetoworkspacesilent "special:vpn,class:.protonvpn-app-wrapped"
+#hyprctl dispatch movetoworkspacesilent "special:vpn,class:proton.vpn.app.gtk"
+hyprctl dispatch 'hl.dsp.window.move({ workspace = "special:vpn", follow = false,window = "class:proton.vpn.app.gtk" })'
 else
- hyprctl dispatch movetoworkspacesilent "+0,class:.protonvpn-app-wrapped"
+hyprctl dispatch 'hl.dsp.window.move({ workspace = "+0", follow = false,window = "class:proton.vpn.app.gtk" })'
+ #hyprctl dispatch movetoworkspacesilent "+0,class:proton.vpn.app.gtk"
 
 fi
